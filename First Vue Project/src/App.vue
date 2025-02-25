b89984146ecc65b419c13beb757702f96ea2bf86
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-02-25 18:19:03 +0100

    dynamic style

--- First Vue Project/src/App.vue
+++ First Vue Project/src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>Compteur : {{ count }}</p>
+  <p :id="`p-${count}`">Compteur : {{ count }}</p>
+
+
+<p :style="{color: count >= 5 ? 'green' : 'red'}">style dynamique</p>
+
+
+
   <div v-if="count >= 5">Bravo vous avez cliqué plus de 5 fois</div>
   <div v-else>Vous avez cliqué moins de cinq fois</div>
   <button v-on:click="increment">Incrémenter</button>
