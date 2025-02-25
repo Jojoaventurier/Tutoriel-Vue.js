2e5186adfc25a5d3a0ad05e53fb8fd02b11450ab
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-02-25 21:40:40 +0100

    v-for

--- First Vue Project/src/App.vue
+++ First Vue Project/src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <p :id="`p-${count}`">Compteur : {{ count }}</p>
 
-
-<p :style="{color: count >= 5 ? 'green' : 'red'}">style dynamique</p>
-
-
+  <p :style="{color: count >= 5 ? 'green' : 'red'}">style dynamique</p>
+  <p :class="{active: count >= 5}">Actif dynamique</p>
 
   <div v-if="count >= 5">Bravo vous avez cliqué plus de 5 fois</div>
   <div v-else>Vous avez cliqué moins de cinq fois</div>
+
   <button v-on:click="increment">Incrémenter</button>
   <button v-on:click="decrement">Décrémenter</button>
+
+  <ul>
+    <li v-for="movie in movies">
+      {{ movie }}
+    </li>
+  </ul>
 </template>
 
 <script setup>
 import {ref} from 'vue'
 
 const count = ref(0)
+const movies = ref([
+  'Matrix',
+  'Alien',
+  'Blade Runner'
+])
 const increment = () => {
   count.value++
 }
