6a9c1ac8cea6bbfeee71bff76fed4719d5e49c01
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-02-26 11:14:43 +0100

    todolist exercise

---
+++ ToDoList/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>ToDoList</h1>
+
+  <ul>
+    <li 
+    v-for="task in taskListTest"
+    :key="task"
+    >
+      {{ task.title }}
+      {{ task.date }} 
+      <button @click="deleteMovie(movie)">Supprimer</button>
+
+    </li>
+  </ul>
+</template>
+
+
+<script setup>
+
+const taskListTest = [
+    { "title": "Acheter la propriété 'Rue de la Paix'", "completed": false, "date": 20240730 },
+    { "title": "Construire un hôtel sur 'Avenue Foch'", "completed": false, "date": 20240730 },
+    { "title": "Éviter la case prison", "completed": false, "date": 20240730 }
+]
+
+</script>
+
+
+
