48ed935cb481c52048cdfaaa95ddcb973b0b2d73
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-02-27 17:36:09 +0100

    fonction personnalisée pour le timer

--- Watchers/src/App.vue
+++ Watchers/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <input type="text" v-model="page.title">
+  Temps écoulé : {{ timer }}
 </template>
 
 <script setup>
 import { ref, watch, watchEffect } from 'vue'
+import { useTimer } from './composable/useTimer.js'
 
+  const timer = useTimer()
   const page = ref({
     title: ''
   })
---
+++ Watchers/src/composable/useTimer.js
@@ -0,0 +1,18 @@
+import { onMounted, onUnmounted, ref} from 'vue';
+
+export function useTimer(initial = 0) {
+    const time = ref(initial)
+
+    let timer
+    onMounted(() => {
+        timer = setInterval(() => {
+            time.value++
+        }, 1_000)
+    })
+
+    onUnmounted(() => {
+        clearInterval(timer);
+    })
+
+    return time
+}
