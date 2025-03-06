8c4b94d920b8c8590196472ab71f16cb82832b9a
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-06 16:15:30 +0100

    question.vue

---
+++ Quizz - Correction/src/components/question.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="question">
+        <h3>{{ question }}</h3>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    question: Object
+})
+</script>
--- Quizz - Correction/src/components/quiz.vue
+++ Quizz - Correction/src/components/quiz.vue
@@ -2,17 +2,19 @@
     <div>
         <h1>{{ quiz.title }}</h1>
         <Progress :value="step" :max="quiz.questions.length - 1"/>
+        <Question :question="question" v-if="question"/>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import Progress from './progress.vue'
 
-defineProps ({
+const props = defineProps ({
     quiz: Object
 })
 
 const step = ref(0)
+const question = computed(() => props.quiz.questions[step.value]) // step est une valeur réactive, il faut donc écrire step.value
 
 </script>
