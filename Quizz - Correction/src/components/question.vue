d3119356407a1b2b70f3ac3555fbe27cadfe47db
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-06 16:30:33 +0100

    ongoing

--- Quizz - Correction/src/components/question.vue
+++ Quizz - Correction/src/components/question.vue
@@ -2,7 +2,12 @@
     <div class="question">
         <h3>{{ question.question }}</h3>
         <ul>
-            <li v-for="choice in question.choices" :key="choice">{{ choice }}</li>
+            <li v-for="(choice, index) in question.choices" :key="choice"> <!--on peut ajouter index dans le for pour le récupérer ensuite et le mettre en id par exemple-->
+                <label :for="`answer${index}`"> <!--on pense bien à lier l'id de l'input au for du label pour l'accessibilité-->
+                    <input :id="`answer${index}`" type="radio" name="answer">
+                    {{ choice }}
+                </label>
+            </li>
         </ul>
     </div>
 </template>
