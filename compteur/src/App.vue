f2d11df63980c1958a10ed258ce04369b59a1af8
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-12 18:41:56 +0100

    ok

--- compteur/src/App.vue
+++ compteur/src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup>
-  import { computed, ref } from 'vue';
+  import { computed, ref, shallowRef, toRaw } from 'vue';
   import { useCounter } from './composable/useCounter.js';
 
   const { count, increment } = useCounter()
@@ -15,9 +15,17 @@
     firstname: 'John',
     lastname: 'Doe'
   }
+
+  const raw2 = reactive({
+    firstname: 'John',
+    lastname: 'Doe'
+  })
+
+
   const person = ref(raw)
   const person2 = shallowRef(raw)
   const person3 = ref(markRaw(raw))
-  console.log(person2.value, person.value === raw)
+  console.log(person3.value, person2.value, person.value === raw)
+  console.log(toRaw(person.value) === raw)
   const double = computed(() => count.value * 2)
 </script>
