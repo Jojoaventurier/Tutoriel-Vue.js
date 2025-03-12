6c586dd80f7a53a1c9323d0b5f65ec834bff8714
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-12 18:06:42 +0100

    homepage ok

---
+++ compteur/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Compteur</h2>
+  <p>
+    Compteur : {{ count }} x 2 = {{ double }}
+  </p>
+  <button @click="increment">Increment</button>
+</template>
+
+<script setup>
+</script>
