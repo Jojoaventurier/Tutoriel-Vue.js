1c2250d93d6b715c7ca190c67bd2c30f9e50fab0
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-12 18:39:34 +0100

    carrying on

--- compteur/src/App.vue
+++ compteur/src/App.vue
@@ -11,10 +11,13 @@
   import { useCounter } from './composable/useCounter.js';
 
   const { count, increment } = useCounter()
-  const person = ref({
+  const raw = {
     firstname: 'John',
     lastname: 'Doe'
-  })
-  console.log(person)
+  }
+  const person = ref(raw)
+  const person2 = shallowRef(raw)
+  const person3 = ref(markRaw(raw))
+  console.log(person2.value, person.value === raw)
   const double = computed(() => count.value * 2)
 </script>
