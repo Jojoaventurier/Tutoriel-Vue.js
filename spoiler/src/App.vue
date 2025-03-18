d7236880bb7ccdf052aed7ebc590270b62a23752
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-18 15:43:23 +0100

    ok

--- spoiler/src/App.vue
+++ spoiler/src/App.vue
@@ -1,33 +1,26 @@
 <template>
   <button @click="toggleSpoiler">Afficher / Masquer le spoiler</button>
 
-  <Transition name="fadeSlide"> <!--VOIR DOCUMENTATION CSS-BASED TRANSITIONS-->
+  <FadeSlideTransition name="fadeSlide"> <!--VOIR DOCUMENTATION CSS-BASED TRANSITIONS-->
     <div v-if="showSpoiler" class="spoiler">
       A la fin de la série, Marc Cunningan meurt !
     </div>
-  </Transition>
+  </FadeSlideTransition>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import FadeSlideTransition from './FadeSlideTransition.vue';
 
 const showSpoiler = ref(false)
 const toggleSpoiler = () => showSpoiler.value = !showSpoiler.value
 </script>
 
-<style>
-.spoiler {
-  padding: 1rem;
-  border: 1px solid #c42626de;
-  transition: .5s;
-}
-.fadeSlide-enter-from {
-  opacity: 0;
-  transform: translateX(10px);
-}
 
-.fadeSlide-leave-to {
-  opacity: 0;
-  transform: translateX(-10px);
-}
+<style>
+  .spoiler {
+    padding: 1rem;
+    border: 1px solid #c42626de;
+    transition: .5s;
+  }
 </style>
---
+++ spoiler/src/FadeSlideTransition.vue
@@ -0,0 +1,22 @@
+<template>
+    <Transition name="fadeSlide">
+        <slot></slot>
+    </Transition>
+</template>
+
+<style>
+.fadeSlide-enter-active,
+fadeSlide-leave-active {
+    transition: .5;
+}
+
+.fadeSlide-enter-from {
+  opacity: 0;
+  transform: translateX(10px);
+}
+
+.fadeSlide-leave-to {
+  opacity: 0;
+  transform: translateX(-10px);
+}
+</style>
