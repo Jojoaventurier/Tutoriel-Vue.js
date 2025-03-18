7db56ee5ee385edf9c2fa5d389e50eda0e8def4d
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-18 16:04:39 +0100

    initialisation ok

---
+++ List Animation/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <form role="group" @submit.prevent="addMovie">
+    <input type="text" v-model="movie">
+    <button :disabled="movie.length === 0 ">Ajouter</button>
+  </form>
+  <ul>
+    <li v-for="movie in movies" :key="movie">
+      {{ movie }}
+      <button class="secondary" @click="removeMovie(Movie)">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showSpoiler = ref(false)
+const movies = ref([
+  "Les Evadés",
+  "Le Parrain",
+  "Matrix",
+  "Pulp Fiction",
+  "Alien",
+  "Terminator 2"
+]);
+
+const movie = ref('')
+const addMovie = () => {
+  
+}
+</script>
---
+++ List Animation/src/FadeSlideTransition.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="relative">
+        <Transition name="fadeSlide" mode="out-in"> <!--Avec mode="out-in", on évite le petit décalage d'animation, la suivante commence quand la première est finie-->
+            <slot></slot>
+        </Transition>
+<!--On a aussi appear :
+    <Transition name="fadeSlide" appear>
+        <slot></slot>
+    </Transition>
+-->
+    </div>
+</template>
+
+<style>
+.relative {
+    position: relative;
+}
+
+.fadeSlide-enter-active,
+.fadeSlide-leave-active {
+    transition: .5s;
+}
+
+.fadeSlide-leave-active {
+    position: absolute;
+    width: 100%;
+}
+
+.fadeSlide-enter-from {
+  opacity: 0;
+  transform: translateX(10px);
+}
+
+.fadeSlide-leave-to {
+  opacity: 0;
+  transform: translateX(-10px);
+}
+</style>
