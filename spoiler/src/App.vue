5b4d1d78efc61621898b3b999efbb8e49586356d
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-18 15:25:42 +0100

    initializing project

--- spoiler/src/App.vue
+++ spoiler/src/App.vue
@@ -1,47 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="toggleSpoiler">Afficher / Masquer le spoiler</button>
+  <div v-if="showSpoiler" class="spoiler">
+    A la fin de la série, Marc Cunningan meurt !
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import { ref } from 'vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+const showspoiler = ref(false)
+const toggleSpoiler = () => showspoiler.value = !showspoiler.value
+</script>
