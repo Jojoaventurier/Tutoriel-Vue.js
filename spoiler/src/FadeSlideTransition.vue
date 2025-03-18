d51e69afab516411f2740b969819d7df4bd06e5f
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-18 15:52:00 +0100

    ok

--- spoiler/src/FadeSlideTransition.vue
+++ spoiler/src/FadeSlideTransition.vue
@@ -1,13 +1,24 @@
 <template>
-    <Transition name="fadeSlide" mode="out-in"> <!--Avec mode="out-in", on évite le petit décalage d'animation, la suivante commence quand la première est finie-->
-        <slot></slot>
-    </Transition>
+    <div class="relative">
+        <Transition name="fadeSlide" mode="out-in"> <!--Avec mode="out-in", on évite le petit décalage d'animation, la suivante commence quand la première est finie-->
+            <slot></slot>
+        </Transition>
+    </div>
 </template>
 
 <style>
+.relative {
+    position: relative;
+}
+
 .fadeSlide-enter-active,
 .fadeSlide-leave-active {
     transition: .5s;
+}
+
+.fadeSlide-leave-active {
+    position: absolute;
+    width: 100%;
 }
 
 .fadeSlide-enter-from {
