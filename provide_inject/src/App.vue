1abdc51e57eacd52f9b81d5200cef97f83c09828
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-03-27 14:53:23 +0100

    provide/inject

---
+++ provide_inject/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <article>
+    <h1>Article</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla ducimus asperiores, veniam hic recusandae praesentium laboriosam! Alias, sit. A eligendi doloremque cupiditate veniam reiciendis animi ipsum incidunt, delectus quasi? Nam.
+    </p>
+    <button>
+      
+    </button>
+  </article>
+</template>
